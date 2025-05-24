358390250e28bbd544640c2767b3382c3aec5bff
Marcelo Vilas Boas Correa Filho <51462138+marcelovbcfilho@users.noreply.github.com>
2025-05-24 14:00:55 -0300

    fix: swift package naming issue

--- packages/window_manager/macos/window_manager/Package.swift
+++ packages/window_manager/macos/window_manager/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "window_manager",
+            name: "window-manager",
             targets: ["window_manager"]
         )
     ],
